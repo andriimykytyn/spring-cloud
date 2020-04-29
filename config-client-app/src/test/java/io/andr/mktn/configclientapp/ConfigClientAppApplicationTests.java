57ed9a875ba4db28b794920d59db2f7a0fb8253d
andriimykytyn <andriymykytyn@yahoo.com>
2020-04-30 00:01:18 +0300

    first commit

---
+++ config-client-app/src/test/java/io/andr/mktn/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.andr.mktn.configclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
